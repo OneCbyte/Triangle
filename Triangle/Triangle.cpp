d37134827b778890a01fb97eaefb9a496587a0ab
Michael <micaelmicael228@gmail.com>
2026-09-24 02:54:26 +0300

    Первый коммит

---
+++ Triangle/Triangle.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int a, h;
+    std::cout << "Введите основание треугольника: \n";
+    std::cin >> a;
+    std::cout << "Введите высоту треугольника: \n";
+    std::cin >> h;
+    std::cout << "Площадь - " << ((double)a*h/2);
+    return 0;
+}
+
